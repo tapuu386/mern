99f68d5f73d4211989b1c23e56a42be240805fe0
Tapasya <142429296+tapuu386@users.noreply.github.com>
2024-04-05 09:41:10 +0530

    Add files via upload

---
+++ day3/index.js
@@ -0,0 +1,27 @@
+// var allow redeclaration 
+// function env{
+//     for(i=0;i<=5;i++){
+//         console.log
+//     }
+
+
+// type cohesion
+
+// function myname(x="nothing is here"){
+// console.log(x);
+// }
+//  const s= "hello";
+// myname(s);
+// function sum (a,b){
+// if(a&&b){
+//     console.log()
+// }
+
+// console.log(a+b);
+
+// }function declaration
+// 
+const x=prompt("please teell the property to know");
+console.log()
+
+
