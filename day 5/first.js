aee82797e14848aa610f27533172ec4992937c5b
Tapasya <142429296+tapuu386@users.noreply.github.com>
2024-04-10 12:28:28 +0530

    Add files via upload

---
+++ day 5/first.js
@@ -0,0 +1,64 @@
+// function printpreety(){
+//     console.log("preetyprint");
+//     let ans =2+3;
+// console.log(ans);
+// console.log("preetyprint");
+
+// }
+// setTimeout(() => {
+    
+// }, timeout);
+// function myDisplayer(some) {
+//     document.getElementById("demo").innerHTML = some;
+//   }
+  
+//   let myPromise = new Promise(function(myResolve, myReject) {
+//     let x = 0;
+  
+//   // The producing code (this may take some time)
+  
+//     if (x == 0) {
+//       myResolve("OK");
+//     } else {
+//       myReject("Error");
+//     }
+//   });
+  
+//   myPromise.then(
+//     function(value) {myDisplayer(value);},
+//     function(error) {myDisplayer(error);}
+//   );
+  
+  // console.log("......appp started...");
+  // function callAPI(){
+  //   fetch("https://newsapi.org/v2/everything?q=tesla&from=2024-03-08&sortBy=publishedAt&apiKey=API_KEY)")
+  //   .then((res)=>{
+  //     // return res.json()
+  //     // console.log(res)
+  //   })
+  //   .then((data)=>
+  //     // return res.json()
+  //     renderUI(data))
+  // }
+  // // callAPI()
+  // function renderUI(data){
+  //   console.log(data);
+
+
+  
+  //   const root=document.getElementById("root")
+  //   const div=document.createElement("div")
+  //   div.innerText="card";
+  //   root.appendChild(div);
+
+
+  // // }
+  
+  // // // renderUI();
+  // // callAPI();
+  // const articles = data.articles;
+  // const ar= articles
+// function fetchaaAPI(){
+//   const='https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=2GSPQSCGKSNM7Y5GDQEH6VSFM)
+// }
+
